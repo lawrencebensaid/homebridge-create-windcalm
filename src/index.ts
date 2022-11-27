9f331ace4ae2c4fe48eb9541bcdc5d49ff8d0056
Lawrence Bensaid <lawrencebensaid@icloud.com>
2022-11-27 12:47:33 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import { API } from 'homebridge';
 import { CREATEWindcalm } from './accessory';
 
 export = (api: API) => {
-  api.registerAccessory('homebridge-create-windcalm', CREATEWindcalm);
+  api.registerAccessory('CREATEWindcalm', CREATEWindcalm);
 };
